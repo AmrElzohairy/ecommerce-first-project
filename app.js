29a02f795251748bec3bd157da6097217ea87f42
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-13 13:32:23 +0200

    add mongo connections

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const connectDB = require('./connections/db.connection');
 const app = express();
+
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 const CURRENT_ENV = process.env.CURRENT_ENV || 'development';
