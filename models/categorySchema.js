73a0a3b1901e3229076729a786c839d440069524
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-14 13:46:08 +0200

    ref catogry model

--- models/categorySchema.js
+++ models/categorySchema.js
@@ -4,12 +4,20 @@ const categorySchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: true,
-            unique: true,
-
+            required: [true, 'Category name is required'],
+            unique: [true, 'Category name must be unique'],
+            minlength: [3, 'Category name must be at least 3 characters long'],
+            maxlength: [50, 'Category name must be at most 50 characters long'],
+        },
+        slug: {
+            type: String,
+            lowercase: true,
         }
-    })
+    }, {
+    timestamps: true,
+}
+);
 
-    let Category = mongoose.model('Category', categorySchema);
+let Category = mongoose.model('Category', categorySchema);
 
-    module.exports = Category;
+module.exports = Category;
