7bdebbb582ce1f9660c703b31f19846902d40ae2
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-14 19:06:44 +0200

    add category validations

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { getAllCategories, createCategory, getCategoryById, updateCategory, deleteCategory } = require('../controllers/category.controller');
-const { getCategoryByIdValidator } = require('../utils/validations/categoryValidations');
+const { getCategoryByIdValidator, updateCategoryValidator, deleteCategoryValidator, createCategoryValidator } = require('../utils/validations/categoryValidations');
 
-router.route('/').get(getAllCategories).post(createCategory);
+router.route('/')
+    .get(getAllCategories)
+    .post(createCategoryValidator, createCategory);
 
-router.route('/:categoryId').get(getCategoryByIdValidator, getCategoryById).put(updateCategory).delete(deleteCategory);
+router.route('/:categoryId')
+    .get(getCategoryByIdValidator, getCategoryById)
+    .put(updateCategoryValidator, updateCategory)
+    .delete(deleteCategoryValidator, deleteCategory);
 
 module.exports = router;
--- utils/validations/categoryValidations.js
+++ utils/validations/categoryValidations.js
@@ -1,12 +1,34 @@
 const { check } = require('express-validator');
 const validationMiddleware = require('../../middlewares/validatorMiddleware');
+const { create } = require('../../models/categorySchema');
 
 getCategoryByIdValidator = [
     check('categoryId').isMongoId().withMessage('Invalid category id'),
     validationMiddleware
 ];
 
+updateCategoryValidator = [
+    check('categoryId').isMongoId().withMessage('Invalid category id'),
+    validationMiddleware
+];
+
+deleteCategoryValidator = [
+    check('categoryId').isMongoId().withMessage('Invalid category id'),
+    validationMiddleware
+];
+
+createCategoryValidator = [
+    check('name').notEmpty()
+        .withMessage('Category name is required')
+        .isLength({ min: 3 }).withMessage('Category name must be at least 3 characters long')
+        .isLength({ max: 32 }).withMessage('Category name must be at most 32 characters long'),
+    validationMiddleware
+];
+
 
 module.exports = {
-    getCategoryByIdValidator
+    getCategoryByIdValidator,
+    updateCategoryValidator,
+    deleteCategoryValidator,
+    createCategoryValidator
 }
