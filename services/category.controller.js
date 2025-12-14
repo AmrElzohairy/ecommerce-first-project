7a0e1eaa482b11218c27b076d9b95fb3315ac12c
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-14 14:34:54 +0200

    add getAllCategories

--- services/category.controller.js
+++ services/category.controller.js
@@ -3,9 +3,16 @@ const slugify = require('slugify')
 const asyncHandler = require('express-async-handler')
 
 
-let getAllCategories = async (req, res) => {
-
-};
+let getAllCategories = asyncHandler(async (req, res) => {
+  let categories = await categoryModel.find({});
+  res.status(200).json({
+    status: 'success',
+    results: categories.length,
+    data: {
+      "categories": categories
+    },
+  });
+});
 
 let createCategory = asyncHandler(async (req, res) => {
   const name  = req.body.name;
