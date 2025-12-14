18c18399dcc39af8467118314f3c685cd0b0c7a6
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-14 17:44:06 +0200

    ref

--- connections/db.connection.js
+++ connections/db.connection.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
 
   await mongoose.connect(process.env.DB_URI);
   console.log('✅ Connected to MongoDB successfully');
-  // } catch (err) {
-  //   console.error('❌ MongoDB connection error:', err.message);
-  //   process.exit(1);
-  // }
+
 };
 
 module.exports = connectDB;
