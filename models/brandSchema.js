5da0ef2091e7b3b4c3e7750e0caf6bc11752d4f2
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-15 18:12:37 +0200

    add brandSchema

---
+++ models/brandSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const brandSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Brand name is required'],
+            unique: [true, 'Brand name must be unique'],
+            trim: true,
+            minlength: [3, 'Brand name must be at least 3 characters long'],
+            maxlength: [50, 'Brand name must be at most 50 characters long'],
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+        },
+        image: {
+            type: String,
+        }
+    }, {
+    timestamps: true,
+}
+)
+let Brand = mongoose.model('Brand', brandSchema);
+
+module.exports = Brand;
