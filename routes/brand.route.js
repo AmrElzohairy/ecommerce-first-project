aabee8c3f7cf9cff5bd4dac59c6e08d2a6947280
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-15 18:28:59 +0200

    add brand validations

--- routes/brand.route.js
+++ routes/brand.route.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { getAllBrands, getBrandById, createBrand, deleteBrand, updateBrand } = require('../controllers/brand.controller');
-
+const { getBrandByIdValidator,
+    updateBrandValidator,
+    deleteBrandValidator,
+    createBrandValidator } = require("../utils/validations/brandValidations");
 router.route('/')
     .get(getAllBrands)
-    .post(createBrand)
+    .post(createBrandValidator, createBrand)
 
 
 router.route('/:brandId')
-    .get(getBrandById)
-    .put(updateBrand)
-    .delete(deleteBrand)
+    .get(getBrandByIdValidator, getBrandById)
+    .put(updateBrandValidator, updateBrand)
+    .delete(deleteBrandValidator, deleteBrand)
 
 
 module.exports = router;
---
+++ utils/validations/brandValidations.js
@@ -0,0 +1,33 @@
+const { check } = require('express-validator');
+const validationMiddleware = require('../../middlewares/validatorMiddleware');
+
+const getBrandByIdValidator = [
+    check('brandId').isMongoId().withMessage('Invalid Brand id'),
+    validationMiddleware
+];
+
+const updateBrandValidator = [
+    check('brandId').isMongoId().withMessage('Invalid Brand id'),
+    validationMiddleware
+];
+
+const deleteBrandValidator = [
+    check('brandId').isMongoId().withMessage('Invalid Brand id'),
+    validationMiddleware
+];
+
+const createBrandValidator = [
+    check('name').notEmpty()
+        .withMessage('Brand name is required')
+        .isLength({ min: 3 }).withMessage('Brand name must be at least 3 characters long')
+        .isLength({ max: 32 }).withMessage('Brand name must be at most 32 characters long'),
+    validationMiddleware
+];
+
+
+module.exports = {
+    getBrandByIdValidator,
+    updateBrandValidator,
+    deleteBrandValidator,
+    createBrandValidator
+}
