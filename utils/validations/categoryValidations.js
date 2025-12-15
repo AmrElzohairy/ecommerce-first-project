235043fff1a340032d5613232e5e7201fa65cfc1
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-15 03:30:39 +0200

    ref

--- utils/validations/categoryValidations.js
+++ utils/validations/categoryValidations.js
@@ -1,23 +1,22 @@
 const { check } = require('express-validator');
 const validationMiddleware = require('../../middlewares/validatorMiddleware');
-const { create } = require('../../models/categorySchema');
 
-getCategoryByIdValidator = [
+const getCategoryByIdValidator = [
     check('categoryId').isMongoId().withMessage('Invalid category id'),
     validationMiddleware
 ];
 
-updateCategoryValidator = [
+const updateCategoryValidator = [
     check('categoryId').isMongoId().withMessage('Invalid category id'),
     validationMiddleware
 ];
 
-deleteCategoryValidator = [
+const deleteCategoryValidator = [
     check('categoryId').isMongoId().withMessage('Invalid category id'),
     validationMiddleware
 ];
 
-createCategoryValidator = [
+const createCategoryValidator = [
     check('name').notEmpty()
         .withMessage('Category name is required')
         .isLength({ min: 3 }).withMessage('Category name must be at least 3 characters long')
