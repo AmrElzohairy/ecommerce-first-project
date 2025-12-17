fa87358e36d45a413631dbea3695da1ba805a6d3
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-17 13:44:59 +0200

    add sorting

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,21 +2,47 @@ const Product = require('../models/productSchema');
 const slugify = require('slugify')
 const asyncHandler = require('express-async-handler')
 const ApiError = require('../utils/apiError');
+const qs = require('qs');
 
 
 let getProducts = asyncHandler(async (req, res) => {
+    //filter
+    let queryObj = qs.parse(req.query);
+    let excludeFields = ['page', 'limit', 'sort', 'fields'];
+    excludeFields.forEach(v => delete queryObj[v]);
+    // Apply filtration using [gte, gt, lte, lt]
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+
+
+
+    //pagination
     let page = parseInt(req.query.page) || 1;
-    let limit = parseInt(req.query.limit) || 5;
+    let limit = parseInt(req.query.limit) || 50;
     const skip = (page - 1) * limit;
-    let allProductsLength = await Product.countDocuments();
-    let products = await Product.find({})
+
+    //build query
+    let mongooseQuery = Product.find(JSON.parse(queryStr))
         .limit(limit)
         .skip(skip)
         .populate('category', { __v: 0 })
         .populate('subCategory', { __v: 0 });
+
+    //sorting
+    if (req.query.sort) {
+        let sortBy = req.query.sort.split(',').join(' ');
+        mongooseQuery = mongooseQuery.sort(sortBy);
+    } else {
+        mongooseQuery = mongooseQuery.sort('-createdAt');
+    }
+
+
+    let products = await mongooseQuery;
+
     res.status(200).json({
         status: 'success',
-        results: allProductsLength,
+        results: products.length,
         data: {
             "products": products
         },
