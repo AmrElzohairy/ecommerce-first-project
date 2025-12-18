b13a329624707c0741472fbabd1ee94b41de9ebb
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-18 14:54:56 +0200

    add user schema

---
+++ models/userSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true
+    },
+    slug: {
+        type: String,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    profileImg: String,
+    password: {
+        type: String,
+        required: true,
+        minlenght: [6, "Password must be at least 6 characters long"]
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, { timestamps: true },);
+
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
