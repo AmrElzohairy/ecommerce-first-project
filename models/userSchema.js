59a493bc7dc31768cca3bb2adb5845c44e7c0e56
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-18 15:37:10 +0200

    ref user schema

--- models/userSchema.js
+++ models/userSchema.js
@@ -31,6 +31,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['user', 'admin'],
         default: 'user'
+    },
+    active: {
+        type: Boolean,
+        default: true
     }
 }, { timestamps: true },);
 
