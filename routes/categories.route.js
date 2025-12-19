8005f2238bb80fd5c349b1335e3e3af51528af9f
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-19 23:05:59 +0200

    add token check

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { getAllCategories, createCategory, getCategoryById, updateCategory, deleteCategory } = require('../controllers/category.controller');
 const { getCategoryByIdValidator, updateCategoryValidator, deleteCategoryValidator, createCategoryValidator } = require('../utils/validations/categoryValidations');
+const auth = require('../controllers/auth.controller');
 
 router.route('/')
     .get(getAllCategories)
-    .post(createCategoryValidator, createCategory);
+    .post(
+        auth.protect,
+        createCategoryValidator, createCategory);
 
 router.route('/:id')
     .get(getCategoryByIdValidator, getCategoryById)
