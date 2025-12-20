ff170f290f71f583325799e5dfcd57ddca9abb8c
Amr El-Zohairy <aelzohairy9@gmail.com>
2025-12-20 14:06:23 +0200

    add permission middleware

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const { getAllCategories, createCategory, getCategoryById, updateCategory, deleteCategory } = require('../controllers/category.controller');
 const { getCategoryByIdValidator, updateCategoryValidator, deleteCategoryValidator, createCategoryValidator } = require('../utils/validations/categoryValidations');
 const auth = require('../controllers/auth.controller');
+const roles = require('../utils/userRoles');
+
 
 router.route('/')
     .get(getAllCategories)
     .post(
         auth.protect,
+        auth.allowedTo(roles.ADMIN, roles.Manager),
         createCategoryValidator, createCategory);
 
 router.route('/:id')
--- utils/userRoles.js
+++ utils/userRoles.js
@@ -1,6 +1,7 @@
 const roles = {
     ADMIN: 'admin',
     USER: 'user',
+    Manager: 'manager'
 }
 
 module.exports = roles
